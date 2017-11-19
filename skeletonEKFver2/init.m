1feb7dd274d68ca9cac5e04eef5fe9d6e71f3b2a
duyipai <yipai.du@outlook.com>
2017-11-19 21:51:52 +0100

    final version

--- skeletonEKFver2/init.m
+++ skeletonEKFver2/init.m
@@ -9,8 +9,9 @@ function [mu,sigma,R,Q,Lambda_M] = init()
 mu = [0;0;0]; % initial estimate of state
 sigma = 1e-10*diag([1 1 1]); % initial covariance matrix
 delta_m = 0.999;
+%delta_m = 1;
 Lambda_M = chi2inv(delta_m,2);
 % Fill In This Part
-R=diag([0.1 0.1 0.1]);
+R=diag([0.001 0.001 0.001]);
 Q=diag([0.2 0.2]);
 end
